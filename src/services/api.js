ba03186b4a2a3a81aafafe6f4f031ec5aeed0198
Dmitriy Alyoshin <valerich1715@gmail.com>
2023-07-04 21:55:30 +0200

    Oy_vey_var_1

---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const fetchImages = async (query, page) => {
+  const response = await axios.get(`https://pixabay.com/api/`, {
+    method: 'get',
+    params: {
+      key: '35614956-4718a491a419b04b9017fc1bc',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 12,
+      page: page,
+    },
+  });
+
+  return response.data;
+};
+
+export default fetchImages;
