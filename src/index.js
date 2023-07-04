e4b1da4e5326e15a7dd48fe229570428d32a3940
Dmitriy Alyoshin <valerich1715@gmail.com>
2023-07-04 21:42:19 +0200

    Start+

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; 
-import App  from 'components/App';
+import { App } from 'components/App';
 import './index.css'; 
 
 
@@ -8,6 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
